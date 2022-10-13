cb954b274fd334f0fda0225e6bb058766e9b05f8
Renato Melo <rdemeloneto2@gmail.com>
2022-10-13 17:12:53 -0300

    first commit

---
+++ airflow-data/dags/elt_dag.py
@@ -0,0 +1,84 @@
+from airflow.utils.edgemodifier import Label
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'NorthwindELT',
+    default_args=default_args,
+    description='A ELT dag for the Northwind ECommerceData',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 10, 12),
+    catchup=True,
+    tags=['example'],
+) as dag:
+    dag.doc_md = """
+        ELT Diária do banco de dados de ecommerce Northwind,
+        começando em 2022-02-07. 
+    """
+
+    extract_postgres_task = BashOperator(
+        task_id='extract_postgres',
+        bash_command='echo "Extracted!" ',
+    )
+
+    extract_postgres_task.doc_md = dedent(
+        """\
+    #### Task Documentation
+
+    Essa task extrai os dados do banco de dados postgres, parte de baixo do step 1 da imagem:
+
+    ![img](https://user-images.githubusercontent.com/49417424/105993225-e2aefb00-6084-11eb-96af-3ec3716b151a.png)
+
+    """
+    )
+
+    load_postgres_data_to_db_task = BashOperator(
+        task_id='load_postgres',
+        bash_command='echo "Loaded postgres data to db!" ',
+    )
+
+    load_postgres_data_to_db_task.doc_md = dedent(
+        """\
+    #### Task Documentation
+
+    Essa task faz load dos dados extraidos do postgres para hd, load para o banco de dados
+    da parte dos dados extraidos do postgres no step 2 da imagem:
+
+    ![img](https://user-images.githubusercontent.com/49417424/105993225-e2aefb00-6084-11eb-96af-3ec3716b151a.png)
+
+    """
+    )
+
+    load_csv_data_to_db_task = BashOperator(
+        task_id='load_csv',
+        bash_command='echo "Loaded csv data to db!" ',
+    )
+
+    load_csv_data_to_db_task.doc_md = dedent(
+        """\
+    #### Task Documentation
+
+    Essa task faz load dos dados csv, load para o banco de dados
+    da parte dos dados extraidos do csv no step 2 da imagem: 
+
+    ![img](https://user-images.githubusercontent.com/49417424/105993225-e2aefb00-6084-11eb-96af-3ec3716b151a.png)
+
+    """
+    )
+
+    
+    
